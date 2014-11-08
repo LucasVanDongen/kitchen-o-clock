f4cec855fb25983714b07fb0d906a4af38c2a6db
Lucas van Dongen <lucas.van.dongen@gmail.com>
2014-11-08 11:57:42 -0300

    Initial push. Has all screens and segues

---
+++ Kitchen o'ClockTests/Kitchen_o_ClockTests.swift
@@ -0,0 +1,36 @@
+//
+//  Kitchen_o_ClockTests.swift
+//  Kitchen o'ClockTests
+//
+//  Created by Lucas on 05/11/14.
+//  Copyright (c) 2014 Departamento B. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class Kitchen_o_ClockTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
