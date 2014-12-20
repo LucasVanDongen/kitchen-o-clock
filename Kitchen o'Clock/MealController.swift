f1a1d67f8613e28f9109923b57b93893397e3717
Lucas van Dongen <lucas.van.dongen@gmail.com>
2014-12-20 18:00:15 -0300

    Meal instructions animate now

--- Kitchen o'Clock/MealController.swift
+++ Kitchen o'Clock/MealController.swift
@@ -53,5 +53,4 @@ class MealController: UIViewController {
         
         self.view.addConstraint(self.imageRatio!)
     }
-
 }
--- Kitchen o'Clock/SlideInfo.swift
+++ Kitchen o'Clock/SlideInfo.swift
@@ -8,15 +8,40 @@
 
 import UIKit
 
-class SlideInfo: UIView {
+class SlideInfo: UIView, UIGestureRecognizerDelegate {
 
     @IBOutlet var cookingInfo: UILabel?
     @IBOutlet var dragTip: UIImageView?
+    @IBOutlet var tabSnapsToSide: NSLayoutConstraint?
+    @IBOutlet var textSnapsToSide: NSLayoutConstraint?
+    
+    let highPriority: Float = 900
+    let lowPriority: Float = 100
+    
     
     required init(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
         // attach touch event to view
         // attach drag event to dragtip
+    }
+    
+    override func touchesBegan(touches: NSSet, withEvent event: UIEvent) {
+        //
+        if self.tabSnapsToSide == nil || self.textSnapsToSide == nil {
+            return
+        }
+        
+        let tabPriority = self.textSnapsToSide!.priority
+        let textPriority = tabPriority == highPriority ? lowPriority : highPriority
+        
+        self.tabSnapsToSide!.priority = tabPriority
+        self.textSnapsToSide!.priority = textPriority
+        
+        UIView.animateWithDuration(0.3, animations: {
+            self.layoutIfNeeded()
+            self.cookingInfo?.layoutIfNeeded()
+            self.dragTip?.layoutIfNeeded()
+        })
     }
 
 }
